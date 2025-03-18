9fd64b149f6535b1b5e550046d708a4b4f65b144
Theo Drakakis <theo@gemjungle.com>
2025-03-18 16:21:27 +1100

    Fixed Schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.1].define(version: 2025_03_18_005838) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,13 +42,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_18_005838) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-
-ActiveRecord::Schema[7.1].define(version: 2025_03_18_005305) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -67,4 +59,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_18_005305) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
