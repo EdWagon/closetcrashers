1d39714593fde22b4efe9e69826e988bddc8e562
Edouard Dillee <ed.dillee@outlook.com>
2025-03-18 14:54:26 +1100

    user model, controller view (devise)

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:show]
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user, notice: "User successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :role, :phone, :address)
+  end
+
+end
