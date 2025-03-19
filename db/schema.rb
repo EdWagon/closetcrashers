174398fd08f7b20a38db2ebc67da610989cfac9a
Edouard Dillee <ed.dillee@outlook.com>
2025-03-19 11:32:16 +1100

    remove user role

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.1].define(version: 2025_03_18_005838) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_18_063349) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,20 +42,12 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_18_005838) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-
-ActiveRecord::Schema[7.1].define(version: 2025_03_18_005305) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "role"
     t.string "first_name"
     t.string "last_name"
     t.integer "phone"
@@ -67,4 +58,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_18_005305) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
