bb7815bf05cbd58d606af8caaf1da094c011c3ee
Pei Chi <lupeggy0608@gmail.com>
2025-03-20 19:16:29 +1300

    fonend stage 3

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
     end
   end
 
+  get "dashboard", to: "pages#dashboard"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
