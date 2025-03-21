704210dc32744cffc6b4fddaccfc0b414c03f4bb
Theo Drakakis <theo@gemjungle.com>
2025-03-21 11:33:09 +1100

    Update to Seed File

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,10 @@
 #
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+#   en
+
+require 'URI'
+require 'open-uri'
 
 puts ("Inserting users")
 User.destroy_all
